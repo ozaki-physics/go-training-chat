29fa957e670af1aa80e7b3db3fbde4cec62ceda8
オザキ <setuna.f.masato@gmail.com>
2021-01-30 14:25:47 +0900

    失敗するというテストが通るようになった

--- go-mount-dir/src/github.com/ozaki-physics/go-training-chat/pkg/trace/tracer.go
+++ go-mount-dir/src/github.com/ozaki-physics/go-training-chat/pkg/trace/tracer.go
@@ -1,6 +1,14 @@
 package trace
 
+import (
+	"io"
+)
+
 // コード内の出来事を記録できるオブジェクトを表すインタフェース
 type Tracer interface {
 	Trace(...interface{})
+}
+
+func New(w io.Writer) Tracer {
+	return nil
 }
--- go-mount-dir/src/github.com/ozaki-physics/go-training-chat/pkg/trace/tracer_test.go
+++ go-mount-dir/src/github.com/ozaki-physics/go-training-chat/pkg/trace/tracer_test.go
@@ -1,9 +1,20 @@
 package trace
 
 import (
+	"bytes"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
-	t.Error("まだテストを作成していません")
+	// 出力されるデータは bytes.Buffer に保持されている
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("New からの戻り値が nil です")
+	} else {
+		tracer.Trace("hello! trace パッケージ")
+		if buf.String() != "hello! trace パッケージ\n" {
+			t.Errorf("'%s'という誤った文字列が出力されました", buf.String())
+		}
+	}
 }
