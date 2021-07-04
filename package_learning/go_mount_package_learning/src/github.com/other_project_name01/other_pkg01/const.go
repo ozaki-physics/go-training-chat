7570b834c34f77da9255c6128b920a05cff49d5e
オザキ <setuna.f.masato@gmail.com>
2021-07-05 00:57:01 +0900

    モジュール構成は別リポジトリに移した

--- package_learning/go_mount_package_learning/src/github.com/other_project_name01/other_pkg01/const.go
+++
@@ -1,4 +0,0 @@
-package other_pkg01
-
-var Other_project_name01_other_pkg01 string = "other_project_name01_other_pkg01_big"
-var other_project_name01_other_pkg01 string = "other_project_name01_other_pkg01_small"
